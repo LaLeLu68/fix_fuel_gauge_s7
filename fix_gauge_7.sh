85974adfd095776acef1fe7ad9c2454ea06fc9e8
LaLeLu68 <67292660+LaLeLu68@users.noreply.github.com>
2020-07-01 00:26:05 +0200

    Add files via upload
    Download and type in terminal sh fix_gauge_7.sh to run the script.
    
    Befor u use the script, charge ur phone to 100% and do NOT unplug the AC. 
    
    U do this on ur own risk.

---
+++ fix_gauge_7.sh
@@ -0,0 +1,10 @@
+# be carefull just use after charging ur battery up to 100% and the ac os plugged in
+# after running this script turn off your phone
+# wait until u see the % of battery and hit the power button untill ur phone starts
+# keep the ac plugged
+# 
+# this is for the s7 flat herolte 
+# u do this on ur own risk
+# LaLeLu68 2020
+
+echo "1" > /sys/devices/battery/power_supply/battery/fg_reset_cap
